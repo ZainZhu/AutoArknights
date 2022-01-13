3fbea080aa28b3c9a585a835877d24fffb4e8294
qianjiaqiu <qianjq@yusur.tech>
2022-01-13 10:22:39 +0800

    b0.1.0 - 01_13 1022 01

---
+++ source/abc.py
@@ -0,0 +1,27 @@
+# 定义行为类规则 方便联合开发
+# 所有行为均需要有四个基础方法
+#     待机时间 | waiting_time
+#     获取信息 | getting_information
+#     计算操作 | instruction_operation
+#     执行操作 | execute_action
+import abc
+
+
+class BaseAction(metaclass=abc.ABCMeta):
+    @abc.abstractmethod()
+    def waiting_time(self):
+        pass
+
+    @abc.abstractmethod()
+    def getting_information(self):
+        pass
+
+    @abc.abstractmethod()
+    def instruction_operation(self):
+        pass
+
+    @abc.abstractmethod()
+    def execute_action(self):
+        pass
+    
+    ...
